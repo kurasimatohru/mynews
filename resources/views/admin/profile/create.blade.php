02fff77042bf980f31f2c7fa5554d8c57e3f8046
kurashimatohru <jbtmax77@yahoo.co.jp>
2020-09-27 09:29:25 +0000

    PHP/Laravel 11

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,13 +1,17 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Comatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        
-        <title>Profile create</title>
-    </head>
-    <body>
-        <h1>プロフィール作成画面</h1>
-    </body>
-</html>
+{{-- layouts/profile.blade.phpを読み込む　--}}
+@extends('layouts.admin')
+
+
+{{-- profile.blade.phpの@yield('title')に'My プロフィール'を埋め込む　--}}
+@section('title','My プロフィール')
+
+{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む　--}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>My プロフィール</h2>
+            </div>
+        </div>
+    </div>
+@endsection
